9927a01de0309beaa2a1cb571fcf39915b17048f
cakasuma <amammustofa@gmail.com>
2023-11-23 20:58:53 +0800

    feat: add simple server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hello')
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    response.end('Hello');
+})
+
+server.listen(80, () => {
+    console.log('server jalan')
+});
